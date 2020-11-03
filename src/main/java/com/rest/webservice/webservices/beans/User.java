6efd092eaeb6c0c3df0ad802b620f95e8263b0c2
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2020-11-03 10:08:36 +0530

    Added content negotiation and swagger api

--- src/main/java/com/rest/webservice/webservices/beans/User.java
+++ src/main/java/com/rest/webservice/webservices/beans/User.java
@@ -1,17 +1,23 @@
 package com.rest.webservice.webservices.beans;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "User API Constraints")
 public class User {
 
     private Integer id;
 
-    @Size(min = 2, message = "Name should have atleast 2 characters")
+    @Size(min = 2, message = "Name should have atleast 2 characters.")
+    @ApiModelProperty(notes = "Name should have atleast 2 characters." )
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "birthDate should be in the past." )
     private Date birthDate;
 
     protected User(){
